4a42f3b9c801733d44a5f138170b7d5caaa8b644
Rashed Alrashed <ralrashed@uco.edu>
2020-10-03 19:32:10 -0500

    Removed extra variable

--- Assets/Scripts/SpaceshipScripts/Spaceship.cs
+++ Assets/Scripts/SpaceshipScripts/Spaceship.cs
@@ -21,8 +21,6 @@ public class Spaceship : MonoBehaviour
     public float leftSolarHP = 100f;
     public float rightSolarHP = 100f;
 
-    public bool isAlive = true;
-
     public Lazer lazer;
 
     // Start is called before the first frame update
