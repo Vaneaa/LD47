9c5955ed9e287a7e8dd55062014ff766525b2510
Rashed Alrashed <ralrashed@uco.edu>
2020-10-04 18:21:53 -0500

    Added HP to HUD

---
+++ Assets/HP.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HP : MonoBehaviour
+{
+    private Spaceship player;
+    private Slider hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hp= GameObject.Find("HP").GetComponent<Slider>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Spaceship>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hp.value = player.HP/100;
+    }
+}
