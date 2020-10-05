c9fa94f8f64e61e602774700ce5f7af76c903481
Rashed Alrashed <ralrashed@uco.edu>
2020-10-04 19:03:28 -0500

    HUD completed

---
+++ Assets/Scripts/HUD Scripts/Energy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Energy : MonoBehaviour
+{
+    private Spaceship player;
+    private Slider energy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        energy= GameObject.Find("Energy").GetComponent<Slider>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Spaceship>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        energy.value = player.energy/100;
+    }
+}
---
+++ Assets/Scripts/HUD Scripts/FuelPanel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FuelPanel : MonoBehaviour
+{
+    private Spaceship player;
+    private Slider fuel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fuel= GameObject.Find("Fuel Panel").GetComponent<Slider>();
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<Spaceship>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        fuel.value = player.fuel/100;
+    }
+}
